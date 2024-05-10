22fd55549a48f017d56d1edafb5b514c3cd4365d
Yuting Lu <kyra.ytlu@gmail.com>
2024-05-10 16:05:35 -0700

    merge&input_completed

--- Grid.js
+++ Grid.js
@@ -14,6 +14,26 @@ export default class Grid {
         })
     }
 
+    get cells() {
+        return this.#cells
+    }
+
+    get cellsByColumn() {
+        return this.#cells.reduce((cellGrid, cell) => {
+            cellGrid[cell.x] = cellGrid[cell.x] || []
+            cellGrid[cell.x][cell.y] = cell
+            return cellGrid
+        }, [])
+    }
+
+    get cellsByRow() {
+        return this.#cells.reduce((cellGrid, cell) => {
+            cellGrid[cell.y] = cellGrid[cell.y] || []
+            cellGrid[cell.y][cell.x] = cell
+            return cellGrid
+        }, [])
+    }
+
     get #emptyCells() {
         return this.#cells.filter(cell => cell.title == null)
     }
@@ -30,11 +50,20 @@ class Cell {
     #x
     #y
     #tile
+    #mergeTile
 
     constructor(cellElement, x, y) {
         this.#cellElement = cellElement
         this.#x = x
         this.#y = y
+    }
+
+    get x() {
+        return this.#x
+    }
+
+    get y() {
+        return this.#y
     }
 
     get tile() {
@@ -46,6 +75,31 @@ class Cell {
         if (value == null) return
         this.#tile.x = this.#x
         this.#tile.y = this.#y
+    }
+
+    get mergeTile() {
+        return this.#mergeTile
+    }
+
+    set mergeTile(value) {
+        this.#mergeTile = value
+        if (value == null) return
+        this.#mergeTile.x = this.#x
+        this.#mergeTile.y = this.#y
+    }
+
+    canAccept(tile) {
+        return (
+            this.#tile == null ||
+            (this.#mergeTile == null && this.tile.value === tile.value)
+        )
+    }
+
+    mergeTiles() {
+        if (this.tile == null || this.mergeTile == null) return
+        this.tile.value = this.tile.value + this.mergeTile.value
+        this.mergeTile.remove()
+        this.mergeTile = null
     }
 }
 function createCellElements(gridElement) {
--- Tile.js
+++ Tile.js
@@ -11,6 +11,10 @@ export default class Tile {
         this.value = value
     }
 
+    get value() {
+        return this.#value
+    }
+
     set value(v) {
         this.#value = v
         this.#tileElement.textContent = v
@@ -28,5 +32,9 @@ export default class Tile {
     set y(value) {
         this.#y = value
         this.#tileElement.style.setProperty("--y", value)
+    }
+
+    remove() {
+        this.#tileElement.remove()
     }
 }
